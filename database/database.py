58753a7fd1a976233357c4b901db47b4f2d71493
Nicolas Perez Caro <nicolas.perez.caro@outlook.com>
2023-06-01 13:23:42 -0500

    Adding the enviroment variables

--- database/database.py
+++ database/database.py
@@ -3,3 +3,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model import Base
 import environ
+
+#Reading enviroment variables
+env = environ.Env()
+environ.Env().read_env()
+
+user = env('postgres_user')
+password = env('postgres_password')
+host = env('postgres_host')
+database = env('postgres_database')
