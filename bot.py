2575218a307b18521683c040c680ab605eb3c720
Nicolas Perez Caro <nicolas.perez.caro@outlook.com>
2023-06-01 12:53:15 -0500

    Creating the function to handle initial messages

--- bot.py
+++ bot.py
@@ -12,3 +12,9 @@ TOKEN = env('BOT_TOKEN')
 
 #Creating the bot
 bot = telebot.TeleBot(TOKEN)
+
+
+#Creating the function to handle initial messages
+@bot.message_handler(commands=['start','hello'])
+def send_welcome(message):
+    bot.reply_to(message, "Hello, how are you doing?")
