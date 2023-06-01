a61f56663753dec9abf3b80284b3975ef3162600
Nicolas Perez Caro <nicolas.perez.caro@outlook.com>
2023-06-01 15:41:48 -0500

    Adding database connection and creating a session

--- database/database.py
+++ database/database.py
@@ -12,3 +12,22 @@ user = env('postgres_user')
 password = env('postgres_password')
 host = env('postgres_host')
 database = env('postgres_database')
+
+
+# Configure the database connection
+database_url = f"postgresql://{user}:{password}{host}:5432/{database}"
+
+# Create the database engine
+engine = create_engine(database_url)
+
+# Create a session factory
+Session = sessionmaker(bind=engine)
+
+def get_session():
+    """
+    Creates a new database session.
+
+    Returns:
+        session: A new session object.
+    """
+    return Session()
